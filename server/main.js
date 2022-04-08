535d11ab3b3cbb8108c719aa4f735ff17e54872b
hschmaiske <ishenriquealbert@gmail.com>
2022-04-08 15:42:45 -0300

    remove credentials

--- server/main.js
+++ server/main.js
@@ -8,8 +8,8 @@ ServiceConfiguration.configurations.upsert(
   {
     $set: {
       loginStyle: 'popup',
-      clientId: 'ec5bc93fb62e77a1d968', // insert your clientId here
-      secret: '49cdd36a3c238ffb00c0694090cf8086f38bd7fd', // insert your secret here
+      clientId: '', // insert your clientId here
+      secret: '', // insert your secret here
     },
   },
 )
